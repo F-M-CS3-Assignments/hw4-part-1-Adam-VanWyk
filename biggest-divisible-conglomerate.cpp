bf6f810ca8d330ac407b47987e7edc1770cf1877
Adam-VanWyk <adam.c.vw@gmail.com>
2025-03-18 15:23:28 -0400

    vec to string copied

--- biggest-divisible-conglomerate.cpp
+++ biggest-divisible-conglomerate.cpp
@@ -6,15 +6,23 @@
 
 using namespace std;
 
-string vec_to_string(vector<int> v){
-    string bdc;
-
-    return bdc;
+string vec_to_string(vector<int> vec){
+	string str = "[";
+	for (unsigned long i = 0; i < vec.size(); i++){
+		//cout << vec[i] << endl;
+		str.append(to_string(vec[i])); 
+		str.append(", ");
+	}
+	str.erase(str.size() - 2, 2);
+	str.append("]");
+	return str; 
 }
 
 
 vector<int> biggest_divisible_conglomerate(vector<int> input){
     vector <int> bdc;
+
+    
 
     return bdc;
 }   
