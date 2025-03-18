058e54b01a30aff7ad41eb272cf7070d5c80b58c
Adam-VanWyk <adam.c.vw@gmail.com>
2025-03-17 21:45:20 -0400

    made files

---
+++ biggest-divisible-conglomerate.cpp
@@ -0,0 +1,20 @@
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include "bdc.h"
+
+using namespace std;
+
+string vec_to_string(vector<int> v){
+    string bdc;
+
+    return bdc;
+}
+
+
+vector<int> biggest_divisible_conglomerate(vector<int> input){
+    vector <int> bdc;
+
+    return bdc;
+}   
