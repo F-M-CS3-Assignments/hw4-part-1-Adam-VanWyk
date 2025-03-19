22814c0ca26c438ba0e004544b0be8547bdb93dd
Adam-VanWyk <adam.c.vw@gmail.com>
2025-03-19 16:33:28 -0400

    trying

--- biggest-divisible-conglomerate.cpp
+++ biggest-divisible-conglomerate.cpp
@@ -31,9 +31,35 @@ vector<int> bdc_helper(vector<int> input){
 	}
 	vector<vector<int>> cands;
 	for (int i = 0; i < input.size() - 1; i ++){
+		int div_pos = -1;
 		vector<int> L;
 		L.push_back(input.at(i));
+
+		for (int j = i; j < input.size() - i; j++){
+			if (input.at(j) % L.at(0) == 0){
+				div_pos = j;
+				break;
+			}
+		}
+		if (div_pos == -1){
+			return L;
+		}
+		vector<int> Rin = sub_vec(input, div_pos, input.size());
+		
+		vector<int> R = bdc_helper(Rin);
+
+		vector<int> cand_v;
+		copy(L.begin(), L.end(), back_inserter(cand_v));
+		copy(R.begin(), R.end(), back_inserter(cand_v));
+
+		cands.push_back(cand_v);
 	}
+	cout << vec_to_string(cands.at(0)) << endl;
 
+	return cands.at(0);
+}
 
+vector<int> sub_vec(vector<int> vec, int start, int end){
+    vector<int> answer(vec.begin() + start, vec.begin() + end);
+    return answer;
 }
