8ec04b6b22c6d62a1a46474208ea043ca6477f21
Adam-VanWyk <adam.c.vw@gmail.com>
2025-03-19 18:17:19 -0400

    Finally realised that the final return is the one that conts and implemented returning the longest item of cands.

--- biggest-divisible-conglomerate.cpp
+++ biggest-divisible-conglomerate.cpp
@@ -32,7 +32,7 @@ vector<int> bdc_helper(vector<int> input){
 
 	vector<vector<int>> cands;
 
-	for (long unsigned i = 0; i < input.size() - 1; i ++){
+	for (long unsigned i = 0; i < input.size(); i ++){
 		int div_pos = input.size();
 
 		vector<int> L;
@@ -41,35 +41,39 @@ vector<int> bdc_helper(vector<int> input){
 		for (long unsigned j = i + 1; j < input.size(); j++){
 			if (input.at(j) % L.at(0) == 0){
 				div_pos = j;
-				cout << "position found with " << vec_to_string(L) << " and " << j << endl;
+				//cout << "position found with " << vec_to_string(L) << " and " << j << endl;
 				break;
 			}
 		}
 		if (div_pos == input.size() || div_pos <= i){
-			return L;
+			cands.push_back(L); // store L with no valid
+			continue;
 		}
 
 		vector<int> Rin = sub_vec(input, div_pos, input.size());
 		
-		if (Rin.size() == 0){
-			return L;
+		if (Rin.empty()){
+			cands.push_back(L);
+		} else {
+			vector<int> R = bdc_helper(Rin);
+
+			vector<int> cand_v;
+			copy(L.begin(), L.end(), back_inserter(cand_v));
+			copy(R.begin(), R.end(), back_inserter(cand_v));
+
+			//cout << vec_to_string(cand_v) << "cand_v printed" << endl;
+			cands.push_back(cand_v);
 		}
-
-		vector<int> R = bdc_helper(Rin);
-
-		vector<int> cand_v;
-		copy(L.begin(), L.end(), back_inserter(cand_v));
-		copy(R.begin(), R.end(), back_inserter(cand_v));
-
-		cands.push_back(cand_v);
 	}
 
 	if (!cands.empty()){
-		for (unsigned long i = 0; i < cands.size(); i++){
-			cout << vec_to_string(cands.at(i)) << endl;
-			cout << "item " << i << endl;
+		vector<int> long_cand = cands.at(0);
+		for ( const auto& cand : cands){
+			if (cand.size() > long_cand.size()){
+				long_cand = cand;
+			}
 		}
-		return cands.at(0);
+		return long_cand;
 	}
 	return input;
 }
