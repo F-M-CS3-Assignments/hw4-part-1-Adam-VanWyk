aa30ac323921e8cf4a58ac848f17322feef69153
Adam-VanWyk <adam.c.vw@gmail.com>
2025-03-28 14:37:03 -0400

    remoeved unnecesary functions?

--- biggest-divisible-conglomerate.cpp
+++ biggest-divisible-conglomerate.cpp
@@ -22,33 +22,6 @@ string vec_to_string(vector<int> vec){
 	return str; 
 }
 
-int find_next_div_pos(vector<int> input, int idx, int mod){
-	for (size_t i = idx; i < input.size(); i++){
-		if (input[i] % mod == 0){
-			return i; // return position of next divisor
-		}
-	}
-	return -1; // no divisor found
-}
-
-vector<int> sub_vec(vector<int> vec, int start){
-	if (start >= vec.size()){
-		return {}; // invalid index
-	}
-    return vector<int> (vec.begin() + start, vec.end());
-
-}
-
-vector<int> longest_vec(vector<vector<int>> cands){
-	vector<int> longest;
-	for (const auto& vec : cands){
-		if (vec.size() > longest.size()){
-			longest = vec;
-		}
-	}
-	return longest;
-}
-
 vector<int> bdc_helper_dynamic(vector<int> input){
     if (input.size() == 0 || input.size() == 1){
         return input;
@@ -56,44 +29,19 @@ vector<int> bdc_helper_dynamic(vector<int> input){
     cout << vec_to_string(input) << endl;
     //cout << input.size() << endl;
 
-    int** index = new int*[input.size()];
+    vector<vector<int>> index(input.size());
     for (unsigned long i = 0; i < input.size(); i++){
-        index[i] = new int[input.size()];
+        index.at(i) = {input.at(i)};
     }
+	for (unsigned long i = 0; i < input.size(); i++){
+		
+
+	}
 
     
 
     return input;
 }
-
-// vector<int> bdc_helper(vector<int> input){
-// 	if (input.size() <= 1){
-// 		return input;
-// 	}
-
-// 	vector<vector<int>> cands;
-
-// 	for (size_t i = 0; i < input.size(); i++){
-// 		vector<int> L = {input[i]};
-// 		set<int> L_set;
-
-// 		int j = find_next_div_pos(input, i + 1, L.back());
-// 		while (j != -1){
-// 			vector<int> Rin = sub_vec(input, j);
-// 			vector<int> R = bdc_helper(Rin); // recursion
-// 			if (!R.empty() && (R.front() % L.back() == 0)){
-// 				L.insert(L.end(), R.begin(), R.end()); // combine
-// 			}
-// 			j = find_next_div_pos(input, j + 1, L.back());
-// 		}
-// 		L_set.insert(L.begin(), L.end());
-// 		L.clear();
-// 		L.assign(L_set.begin(), L_set.end());
-// 		cands.push_back(L); // Add candidate 
-
-// 	}
-// 	return longest_vec(cands);
-// }
 
 vector<int> biggest_divisible_conglomerate(vector<int> input){
     sort(input.begin(), input.end()); // Sort in ascending order
