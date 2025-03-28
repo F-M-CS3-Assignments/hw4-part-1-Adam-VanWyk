8b8a49c1aa80db66619a4bf5be14268b7cb307f5
Adam-VanWyk <adam.c.vw@gmail.com>
2025-03-28 14:44:51 -0400

    done?

--- biggest-divisible-conglomerate.cpp
+++ biggest-divisible-conglomerate.cpp
@@ -22,6 +22,16 @@ string vec_to_string(vector<int> vec){
 	return str; 
 }
 
+int longest_vec(vector<vector<int>> cands){
+	unsigned long longest = 0;
+	for (unsigned long i = 0; i < cands.size(); i++){
+		if (cands.at(i).size() > longest){
+			longest = i;
+		}
+	}	
+	return longest;
+}
+
 vector<int> bdc_helper_dynamic(vector<int> input){
     if (input.size() == 0 || input.size() == 1){
         return input;
@@ -33,14 +43,22 @@ vector<int> bdc_helper_dynamic(vector<int> input){
     for (unsigned long i = 0; i < input.size(); i++){
         index.at(i) = {input.at(i)};
     }
+
+	int curr_element;
 	for (unsigned long i = 0; i < input.size(); i++){
-		
+		curr_element = input.at(i);
+		for (unsigned long j = i + 1; j < input.size(); j++){
+			if (input.at(j)%curr_element==0 && index.at(i).size() >= index.at(j).size()){
+				index.at(j) = {input.at(j)};
+				for (unsigned long k = 0; k < index.at(i).size(); k++){
+					index.at(j).push_back(index.at(i).at(k));
+				}
+			}
+		}
 
 	}
-
-    
-
-    return input;
+	int max_idx = longest_vec(index);
+    return index.at(max_idx);
 }
 
 vector<int> biggest_divisible_conglomerate(vector<int> input){
@@ -48,12 +66,12 @@ vector<int> biggest_divisible_conglomerate(vector<int> input){
     return bdc_helper_dynamic(input);
 }   
 
-int main(){
-    vector<int> tester = {3, 5, 6, 10, 20};
-    vector<int> ans = biggest_divisible_conglomerate(tester);
-    cout << vec_to_string(ans) << endl;
+// int main(){
+//     vector<int> tester = {3, 5, 6, 10, 20};
+//     vector<int> ans = biggest_divisible_conglomerate(tester);
+//     cout << vec_to_string(ans) << endl;
 
-}
+// }
 
 
 
